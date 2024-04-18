19c3de52058f02bcd40223afb9ab0536f8bed658
JCJARAA99 <91167214+Jaraa99@users.noreply.github.com>
2024-04-18 15:04:25 -0500

    Add files via upload

---
+++ proyect/src/main/kotlin/com/example/proyect/ProyectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.proyect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyectApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyectApplication>(*args)
+}
